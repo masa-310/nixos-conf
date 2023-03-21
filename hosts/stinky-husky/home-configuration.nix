44485c11d0daca6d7141223311970aa404039412
Masashi Sato <gmasa.310@gmail.com>
2023-03-21 10:10:31 +0000

    stinky-husky

---
+++ hosts/stinky-husky/home-configuration.nix
@@ -0,0 +1,40 @@
+{ system, nixpkgs, pkgs, home-manager, dotfile-path, hostname, ... }:
+
+{
+  home.file.".Xmodmap" = {
+    text = ''
+    keycode 102 = Super_L
+    '';
+  };
+  modules = {
+    editor.neovim = {
+      enable = true;
+    };
+    program.nodejs = {
+      enable = true;
+      version = 16;
+    };
+    program.starship = {
+      enable = true;
+      shell = "zsh";
+    };
+    program.polybar = {
+      enable = true;
+    };
+    windowManager.xmonad = {
+      enable = true;
+    };
+    terminal.alacritty = {
+      enable = true;
+    };
+    shell.zsh = {
+      enable = true;
+    };
+    program.elm = {
+      enable = true;
+    };
+    # program.haskell = {
+    #   enable = true;
+    # };
+  };
+}
