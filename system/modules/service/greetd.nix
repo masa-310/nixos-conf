331e5b1706f6f6f738ebc60eac86479e398332fd
Masashi SATO <gmasa.310@gmail.com>
2023-09-30 13:11:59 +0900

    `

---
+++ system/modules/service/greetd.nix
@@ -0,0 +1,22 @@
+{ lib, config, pkgs, ... }:
+
+with builtins;
+with lib;
+let self = config.modules.service.greetd;
+in {
+  imports = [];
+  options.modules.service.greetd = {
+    enable = mkEnableOption "greetd";
+  };
+  config = mkIf self.enable {
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd startx";
+          user = "greeter";
+        };
+      };
+    };
+  };
+}
